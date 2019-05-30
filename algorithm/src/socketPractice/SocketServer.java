c32bdc46d3c6c1ad9679639cfd74fe970799d835
Green Jung <38917283+iinee12@users.noreply.github.com>
2019-05-30 10:48:54 +0900

    update
    update

--- algorithm/src/socketPractice/SocketServer.java
+++ algorithm/src/socketPractice/SocketServer.java
@@ -13,20 +13,42 @@ public class SocketServer {
 		// TODO Auto-generated method stub
 		
 		
-		ServerSocket listener = new ServerSocket();
+		ServerSocket listener = new ServerSocket("í¬íŠ¸ë²ˆí˜¸");
 		try {
-			Socket socket = listener.accept();   //client ÀÀ´äÀÌ ¿À±æ ±â´Ù¸°´Ù.
+			Socket socket = listener.accept();   //client ì‘ë‹µì´ ì˜¤ê¸¸ ê¸°ë‹¤ë¦°ë‹¤.
+			
+			InputStream in = null;
+			FileOutputStream out = null;
+			in = socket.getInputStream();
+			DataInputStream din = new DataInputStream(in);
+			int data = din.readInt();
+			String Filename = din.readUTF();
+			out = new FileOutputStream(íŒŒì¼ê°ì²´)
+			int datas = data;
+			byte[] buffer = new byte[1024];
+			int len;
+			
+			for(;data>0;data--){
+			    len = in.read(buffer);
+			    out.write(buffer, 0,len)
+			}
+			out.flush();
+			out.close();
 			
 			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 			
 			Date time = new Date();
 			String time2 = format.format(time);
 			
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);  //¼ÒÄÏ Åë½ÅÀÇ ¾Æ¿ôÇ² ½ºÆ®¸²À» ³»º¸³½´Ù.
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);  //ì†Œì¼“ í†µì‹ ì˜ ì•„ì›ƒí’‹ ìŠ¤íŠ¸ë¦¼ì„ ë‚´ë³´ë‚¸ë‹¤.
 			out.print(time2);
-			socket.close();  //ÀÚ¿ø ¹İ³³
+			socket.close();  //ìì› ë°˜ë‚©
 		}finally{
-			listener.close();  // ÀÚ¿ø ¹İ³³
+			listener.close();  // ìì› ë°˜ë‚©
 		}	
 	}
 }
+
+
+
+
